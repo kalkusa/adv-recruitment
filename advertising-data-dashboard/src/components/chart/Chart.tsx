f9cfc96a7fed01ff857327a6d836a19c706d9c5a
Arkadiusz Kałkus <kalkus@10g.pl>
2023-08-13 19:26:21 +0200

    Refactor d3 chart and introduce useD3Chart hook - Split the d3 chart into smaller, more manageable functions. - Extracted chart generation logic to the useD3Chart hook.

--- advertising-data-dashboard/src/components/chart/Chart.tsx
+++ advertising-data-dashboard/src/components/chart/Chart.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import useTimeSeries from "../../hooks/useTimeSeries";
-import { useEffect, useRef } from "react";
-import { line as d3line, select, scaleTime, extent, scaleLinear, axisBottom, max, axisLeft } from "d3";
-import { ValueInTime } from "../../types/chartTypes";
+import { useRef } from "react";
+import { useD3Chart } from "../../hooks/useD3Chart";
+import { CircularProgress } from "@mui/material";
 
 const Section = styled.section`
   border-width: 1px;
@@ -15,50 +15,7 @@ const Chart = () => {
   const timeSeriesData = useTimeSeries();
   const svgRef = useRef<SVGSVGElement | null>(null);
 
-  useEffect(() => {
-    if (svgRef.current) {
-      const svg = select(svgRef.current);
-
-      // Set dimensions
-      const width = 800;
-      const height = 400;
-      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-
-      // Create scales
-      const xScale = scaleTime()
-        .domain(extent(timeSeriesData, (d) => d.date) as [Date, Date])
-        .range([margin.left, width - margin.right]);
-
-      const yScale = scaleLinear()
-        .domain([0, max(timeSeriesData, (d) => d.value) as number])
-        .nice()
-        .range([height - margin.bottom, margin.top]);
-
-      // Create axes
-      const xAxis = axisBottom(xScale);
-      const yAxis = axisLeft(yScale);
-
-      svg
-        .append("g")
-        .attr("transform", `translate(0,${height - margin.bottom})`)
-        .call(xAxis);
-
-      svg.append("g").attr("transform", `translate(${margin.left},0)`).call(yAxis);
-
-      // Create line
-      const line = d3line<ValueInTime>()
-        .x((d) => xScale(d.date))
-        .y((d) => yScale(d.value));
-
-      svg
-        .append("path")
-        .datum(timeSeriesData)
-        .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
-    }
-  }, [timeSeriesData]);
+  useD3Chart(svgRef, timeSeriesData);
 
   return (
     <Section id="chart">
@@ -68,7 +25,7 @@ const Chart = () => {
           <svg ref={svgRef} width="800" height="400"></svg>
         </div>
       ) : (
-        <div>Loading time series data</div>
+        <CircularProgress disableShrink />
       )}
     </Section>
   );
---
+++ advertising-data-dashboard/src/hooks/useD3Chart.ts
@@ -0,0 +1,89 @@
+import { useEffect } from "react";
+import { line as d3line, select, scaleTime, extent, scaleLinear, scaleOrdinal, axisBottom, max, axisLeft } from "d3";
+import { ValueInTime } from "../types/chartTypes";
+
+const setupScales = (data: ValueInTime[], width: number, height: number, margin: any) => {
+  const xScale = scaleTime()
+    .domain(extent(data, (d) => d.date) as [Date, Date])
+    .range([margin.left, width - margin.right]);
+
+  const yScale = scaleLinear()
+    .domain([0, max(data, (d) => d.value) as number])
+    .nice()
+    .range([height - margin.bottom, margin.top]);
+
+  return { xScale, yScale };
+};
+
+const drawXAxis = (svg: any, xScale: any, height: number, margin: any) => {
+  const xAxis = axisBottom(xScale);
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(xAxis);
+};
+
+const drawYAxis = (svg: any, yScale: any, margin: any) => {
+  const yAxis = axisLeft(yScale);
+  svg.append("g").attr("transform", `translate(${margin.left},0)`).call(yAxis);
+};
+
+const drawLineChart = (svg: any, data: ValueInTime[], xScale: any, yScale: any) => {
+  const line = d3line<ValueInTime>()
+    .x((d) => xScale(d.date))
+    .y((d) => yScale(d.value));
+
+  svg
+    .append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+};
+
+const drawLegend = (svg: any, width: number) => {
+  const color = scaleOrdinal<string, string>().domain(["Clicks"]).range(["#1f77b4"]);
+
+  const legend = svg
+    .append("g")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 10)
+    .attr("text-anchor", "end")
+    .selectAll("g")
+    .data(color.domain())
+    .enter()
+    .append("g")
+    .attr("transform", (d: any, i: number) => `translate(0,${i * 20})`);
+
+  legend
+    .append("rect")
+    .attr("x", width - 19)
+    .attr("width", 19)
+    .attr("height", 19)
+    .attr("fill", color);
+
+  legend
+    .append("text")
+    .attr("x", width - 24)
+    .attr("y", 9.5)
+    .attr("dy", "0.32em")
+    .text((d: any) => d);
+};
+
+export const useD3Chart = (svgRef: React.RefObject<SVGSVGElement>, timeSeriesData: ValueInTime[]) => {
+  useEffect(() => {
+    if (svgRef.current && timeSeriesData.length) {
+      const svg = select(svgRef.current);
+      const width = 800;
+      const height = 400;
+      const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+
+      const { xScale, yScale } = setupScales(timeSeriesData, width, height, margin);
+      drawXAxis(svg, xScale, height, margin);
+      drawYAxis(svg, yScale, margin);
+      drawLineChart(svg, timeSeriesData, xScale, yScale);
+      drawLegend(svg, width);
+    }
+  }, [svgRef, timeSeriesData]);
+};
